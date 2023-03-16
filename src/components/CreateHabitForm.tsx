2b5ec58ae666b048eb725b3284bdd73043105ea6
girlpluscomputer <samantha.ruth.nunez@gmail.com>
2023-03-16 17:31:55 +0000

    Remove unused import

--- src/components/CreateHabitForm.tsx
+++ src/components/CreateHabitForm.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useEffect, useState } from 'react'
+import { FormEvent, useState } from 'react'
 import { Box, Button, Input, Select, Text } from '@chakra-ui/react'
 
 import { WEEK_PROGRESS } from '@/utils/constants'
