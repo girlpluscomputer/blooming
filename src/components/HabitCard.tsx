db3f75bdfbde14ed20257c04b696081a2c8aea86
girlpluscomputer <samantha.ruth.nunez@gmail.com>
2023-04-26 20:37:29 +0100

    add delete habit button

--- src/components/HabitCard.tsx
+++ src/components/HabitCard.tsx
@@ -35,61 +35,62 @@ export const HabitCard = ({ habit }: HabitCardProps) => {
   }
 
   return (
-    <>
-      <Card
-        maxWidth="200px"
-        height="180px"
-        p="16px"
-        borderRadius="20px"
-        bg={completed ? '#e6daf6' : '#fff'}
-        _hover={{
-          border: '2px solid #c8a6f4',
-          cursor: 'pointer'
-        }}
-        onClick={handleClick}
+    <Card
+      maxWidth="200px"
+      height="180px"
+      p="16px"
+      borderRadius="20px"
+      bg={completed ? '#e6daf6' : '#fff'}
+      className="habit-card"
+      role="group"
+      _hover={{
+        border: '2px solid #c8a6f4',
+        cursor: 'pointer'
+      }}
+      onClick={handleClick}
+    >
+      <CardBody
+        p="0"
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
       >
-        <CardBody
-          p="0"
-          display="flex"
-          flexDirection="column"
-          justifyContent="space-between"
-        >
-          <Box display="flex" justifyContent="space-between">
-            <Text color="var(--chakra-colors-gray)" fontWeight="bold">
-              {`${currentDay} / ${totalOfDays}`}
-            </Text>
-            <Box
-              onClick={handleDeleteHabit}
-              bg="white"
-              borderRadius="100%"
-              borderColor="var(--card-border-color)"
-              boxShadow="var(--card-shadow)"
-              width="30px"
-              height="30px"
-              position="absolute"
-              top={-2}
-              right={-2}
-              hidden
-              _hover={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center'
-              }}
-            >
-              <Trash />
-            </Box>
+        <Box display="flex" justifyContent="space-between">
+          <Text color="var(--chakra-colors-gray)" fontWeight="bold">
+            {`${currentDay} / ${totalOfDays}`}
+          </Text>
+          <Box
+            as="button"
+            onClick={handleDeleteHabit}
+            bg="white"
+            borderRadius="100%"
+            borderColor="var(--card-border-color)"
+            boxShadow="var(--card-shadow)"
+            width="30px"
+            height="30px"
+            position="absolute"
+            top={-2}
+            right={-2}
+            display="none"
+            _groupHover={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <Trash />
           </Box>
-          <Box>
-            <Heading fontSize="24px">{title}</Heading>
-            <Text color="var(--chakra-colors-gray)" fontWeight="bold">
-              {description}
-            </Text>
-          </Box>
-          <Box display="flex" justifyContent="space-between">
-            <HabitWeekLogs logs={logs} />
-          </Box>
-        </CardBody>
-      </Card>
-    </>
+        </Box>
+        <Box>
+          <Heading fontSize="24px">{title}</Heading>
+          <Text color="var(--chakra-colors-gray)" fontWeight="bold">
+            {description}
+          </Text>
+        </Box>
+        <Box display="flex" justifyContent="space-between">
+          <HabitWeekLogs logs={logs} />
+        </Box>
+      </CardBody>
+    </Card>
   )
 }
